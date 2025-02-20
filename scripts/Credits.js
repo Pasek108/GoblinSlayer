53400be5431ea20b53ac08ab53a25230a8b4f728
Artur Pas <pasartur108@gmail.com>
2025-02-20 20:30:00 +0100

    contact logic to credits

--- scripts/Credits.js
+++ scripts/Credits.js
@@ -6,6 +6,15 @@ class Credits {
 
     this.close_button = this.contianer.querySelector(".close")
     this.close_button.addEventListener("click", this.hide.bind(this))
+
+    // contact
+    this.contact_container = document.querySelector(".contact-container")
+
+    this.contact_open_button = document.querySelector(".contact-open")
+    this.contact_open_button.addEventListener("click", this.showContactForm.bind(this))
+
+    this.contact_close_button = this.container.querySelector(".contact-close")
+    this.contact_close_button.addEventListener("click", this.hideContactForm.bind(this))
   }
 
   show() {
@@ -14,5 +23,13 @@ class Credits {
 
   hide() {
     this.contianer.style.top = null
+  }
+
+  showContactForm() {
+    this.contact_container.style.display = null
+  }
+
+  hideContactForm() {
+    this.contact_container.style.display = "none"
   }
 }
